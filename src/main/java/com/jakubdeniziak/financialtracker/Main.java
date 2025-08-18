c840fd1e4c0f8fc2c9c8db615714fc9625c5b286
Jakub Deniziak <jakub.deniziak@gmail.com>
2025-08-18 19:42:11 +0200

    feat: connect spring with javafx

--- src/main/java/com/jakubdeniziak/financialtracker/Main.java
+++ src/main/java/com/jakubdeniziak/financialtracker/Main.java
@@ -1,13 +1,12 @@
 package com.jakubdeniziak.financialtracker;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        FinancialTrackerApplication.launch(FinancialTrackerApplication.class, args);
     }
 
 }
