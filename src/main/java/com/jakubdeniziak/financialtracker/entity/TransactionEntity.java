02473cbe4e7104ff0564b837681ed2c779b1f407
Jakub Deniziak <jakub.deniziak@gmail.com>
2025-08-28 10:30:32 +0200

    chore: override utility methods for TransactionEntity

--- src/main/java/com/jakubdeniziak/financialtracker/entity/TransactionEntity.java
+++ src/main/java/com/jakubdeniziak/financialtracker/entity/TransactionEntity.java
@@ -2,9 +2,11 @@ package com.jakubdeniziak.financialtracker.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
 @Entity(name = "Transaction")
 @Table(name = "transactions")
@@ -35,5 +37,21 @@ public class TransactionEntity {
     @ManyToOne
     @JoinColumn(name = "asset_id", nullable = false)
     private AssetEntity asset;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        TransactionEntity that = (TransactionEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 
 }
